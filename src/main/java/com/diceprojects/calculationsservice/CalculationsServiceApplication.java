f25c9726e741f4054a12ea55a30180cfb41139f0
MartinDiCe <jamartindice@gmail.com>
2024-01-24 20:30:43 -0300

    Project Calculations

---
+++ src/main/java/com/diceprojects/calculationsservice/CalculationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diceprojects.calculationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculationsServiceApplication.class, args);
+    }
+
+}
